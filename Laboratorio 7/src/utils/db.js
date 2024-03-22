440464fae786bb55cb7cafa230b7cddb6e3c2562
Carlos Valladares <val221164@uvg.edu.gt>
2024-03-21 19:11:12 -0600

    Creación del index.html para el lab 7

---
+++ Laboratorio 7/src/utils/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'blogdb'
+})
+
+connection.connect((error) => {
+    if (error) throw error
+    console.log('Successfully connected to the database.')
+})
+
+module.exports = connection
